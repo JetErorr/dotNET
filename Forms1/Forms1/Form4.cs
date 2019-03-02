882e6c14971d7dc31c706ce6bb79a887b0f64e8b
JetErorr <jardeshna@gmail.com>
2019-03-02 22:44:58 +0530

    More Forms.!

---
+++ Forms1/Forms1/Form4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Forms1
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = ("txt files|*.txt|(All Files)|(*.*)");
+            if (saveFileDialog1.ShowDialog() != DialogResult.Cancel){
+                File.WriteAllText(saveFileDialog1.FileName, richTextBox1.Text);
+            }
+        }
+    }
+}
